919f77ffc220287fcf92ee9a3c77f0ebdc02a3fa
Darin <darinvu05@gmail.com>
2024-08-05 21:49:00 -0700

    Instead of hiding the Firebase key, I restricted the key to only work on the Ensemble site.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    firebaseConfig : {
+      apiKey: "AIzaSyAuEqeKccr6-WJqBx3hfI5yMHlaGc7qouY",
+      authDomain: "ensemble-163c3.firebaseapp.com",
+      databaseURL: "https://ensemble-163c3-default-rtdb.firebaseio.com",
+      projectId: "ensemble-163c3",
+      storageBucket: "ensemble-163c3.appspot.com",
+      messagingSenderId: "907327283145",
+      appId: "1:907327283145:web:445874e28a016ca1755218",
+      measurementId: "G-63EGM4PL41"
+    }
+  };
+  
